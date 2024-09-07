0f947a266dce1f4d3fa2e95f2db1fcde0bb9e028
hfr4 <30574115+hfr4@users.noreply.github.com>
2024-09-08 01:20:31 +0200

    modify route

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -12,21 +12,21 @@ public class UserConstants
 {
     public static List<UserModel> Users = new List<UserModel>()
     {
-        new UserModel() { 
-            Username = "jason_admin",
+        new UserModel() {
+            Username     = "jason_admin",
             EmailAddress = "jason.admin@email.com",
-            Password = "MyPass_w0rd",
-            GivenName = "Jason",
-            Surname = "Bryant",
-            Role = "Administrator",
+            Password     = "password",
+            GivenName    = "Jason",
+            Surname      = "Bryant",
+            Role         = "Administrator",
         },
-        new UserModel() { 
-            Username = "elyse_seller",
+        new UserModel() {
+            Username     = "elyse_seller",
             EmailAddress = "elyse.seller@email.com",
-            Password = "MyPass_w0rd",
-            GivenName = "Elyse",
-            Surname = "Lambert",
-            Role = "Seller",
+            Password     = "password",
+            GivenName    = "Elyse",
+            Surname      = "Lambert",
+            Role         = "Seller",
         },
     };
 }
